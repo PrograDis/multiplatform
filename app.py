eee4a7e49a1b95c89206c6ab4416b57a2391dcdb
David Tisalema <letisidw@gmail.com>
2025-05-14 16:17:56 -0500

    templates

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    
+    print("Current directory content:", os.listdir())
+    print("Templates content:", os.listdir("templates"))
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
