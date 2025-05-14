12b4abcd3950867456076311aad179d651da6b99
David Tisalema <letisidw@gmail.com>
2025-05-14 15:20:14 -0500

    add template
    index template add

--- app.py
+++ app.py
@@ -5,14 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>¡Bienvenido a Heroku!</h1>
-    <p>mi perro se enoja por todo y se llama Ignis.</p>
-    <button onclick="location.href='https://pagina-heroku.com'">Ir a Heroku</button>
-    <button onclick="location.href='https://pagina-digitalocean.com'">Ir a DigitalOcean</button>
-    <button onclick="location.href='https://multiplatform-production.up.railway.app'">Ir a Railway</button>
-    """
-
+    return render_template("index.html")
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
