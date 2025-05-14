0662676cd150a4ad2de11ea7216651db29f0519b
David Tisalema <letisidw@gmail.com>
2025-05-14 12:53:06 -0500

    config setup
    config for heroku

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+with open("index.html") as f:
+    html_content = f.read()
+
+@app.route("/")
+def home():
+    return render_template_string(html_content)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
