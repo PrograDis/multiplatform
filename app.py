e7270f75e414f3d9e39e0cba67a786b8b0f0a29a
David Tisalema <letisidw@gmail.com>
2025-05-14 13:10:31 -0500

    Allow conections

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template_string
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-with open("index.html") as f:
-    html_content = f.read()
-
 @app.route("/")
 def home():
-    return render_template_string(html_content)
+    return """
+    <h1>¡Página en Railway!</h1>
+    <button onclick="location.href='https://pagina-heroku.com'">Ir a Heroku</button>
+    <button onclick="location.href='https://pagina-digitalocean.com'">Ir a DigitalOcean</button>
+    """
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
