70a2f39c317ba1c946cba8db28561478b1462fda
David Tisalema <letisidw@gmail.com>
2025-05-14 15:49:31 -0500

    render
    render template add

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>¡Pagina en Digital Ocean!</h1>
-    <h2>¡Hola Mundo!</h2>
-    <p>Yo queria seguir artes xd.</p>  
-    <button onclick="location.href='https://pagina-heroku.com'">Ir a Heroku</button>
-    <button onclick="location.href='https://pagina-digitalocean.com'">Ir a DigitalOcean</button>
-    <button onclick="location.href='https://multiplatform-production.up.railway.app'">Ir a Railway</button>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
