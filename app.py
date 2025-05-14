3817521b1bcdee06a17a0af42bf2700601f13a40
David Tisalema <letisidw@gmail.com>
2025-05-14 13:23:36 -0500

    DO changes
    allow conections port

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template_string
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-with open("index.html") as f:
-    html_content = f.read()
-
 @app.route("/")
 def home():
-    return render_template_string(html_content)
+    return """
+    <h1>¡Pagina en Digital Ocean!</h1>
+    <h2>¡Hola Mundo!</h2>
+    <p>Yo queria seguir artes xd.</p>  
+    <button onclick="location.href='https://pagina-heroku.com'">Ir a Heroku</button>
+    <button onclick="location.href='https://pagina-digitalocean.com'">Ir a DigitalOcean</button>
+    <button onclick="location.href='https://multiplatform-production.up.railway.app'">Ir a Railway</button>
+    """
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
